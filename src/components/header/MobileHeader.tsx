fcfef794a1c863b3f4c5c4f60384f9effdecb8d2
Radosław Marek <dotims054@gmail.com>
2024-06-16 00:55:35 +0200

    responsive header/navbar

---
+++ src/components/header/MobileHeader.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Squash as Hamburger } from 'hamburger-react';
+
+const MobileHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Funkcja do zmiany stanu menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-[80px] bg-[#181414] z-50 flex items-center justify-between">
+      <div className="text-white font-bold text-xl pl-4 flex items-center justify-between w-[93.5%]">
+        <Hamburger toggled={isOpen} toggle={toggleMenu} color="#ffffff" size={24} />
+        <span className="ml-4">Telesky Insight</span>
+      </div>
+
+      <div className={`fixed top-0 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} h-full w-[70%] bg-[#181414] text-white p-4 sm:pr-[40px]  transition-transform duration-300 ease-in-out z-40`}>
+        <div className="flex justify-between items-center border-b border-gray-400 py-2">
+          <h2 className="text-md font-bold ml-3">Telesky Insight</h2>
+          <span className='mr-[-10px]'>
+            <Hamburger toggled={isOpen} toggle={toggleMenu} color="#ffffff" size={24} />
+          </span>
+        </div>
+
+        <div className='mt-[15%]'>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">Strona główna</a>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">Teleskop</a>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">Odczyty</a>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">O projekcie</a>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">Członkowie</a>
+          <a href="#" className="block py-4 hover:bg-[#283593] px-4 border-b border-gray-700">Sponsorzy</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileHeader;
---
+++ src/components/header/StandardHeader.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import logo from "../../assets/logos.png"; 
+
+const StandardHeader = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 10) { 
+          setIsScrolled(true);
+        } else {
+          setIsScrolled(false);
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
+  return (
+    <div className="bg-[#181414] text-white fixed top-0 left-1/2 transform -translate-x-1/2 w-[100%] xl:w-[85%] max-w-screen-xl z-50 rounded-bl-xl rounded-br-lg">
+      <div className="flex justify-center items-center h-[80px] relative">
+        <div className="w-full px-4 sm:px-6 md:px-3 xl:px-8">
+          <div className="flex items-center justify-between py-2">
+            <div className="flex items-center justify-start px-1">
+              <button className="text-white md:hidden">
+                <i className="fas fa-bars"></i>
+              </button>
+              <div className={` flex-col md:flex-row md:flex xl:space-x-10 space-x-3`}>
+                <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                  Strona główna
+                </a>
+                <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                  Teleskop
+                </a>
+                <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                  Odczyty
+                </a>
+              </div>
+            </div>
+
+            <img src={logo} alt="Logo" className={`absolute transition-all duration-1000 cubic-bezier(0.25, 0.1, 0.25, 1) top-${isScrolled ? '[6px]' : '[15px]'} left-1/2 transform -translate-x-1/2 ${isScrolled ? 'lg:w-[80px] w-[75px]' : 'lg:w-[120px] w-[105px]'} z-50 bg-[#181414] p-${isScrolled ? '0' : '3'} rounded-bl-xl rounded-br-xl`} />
+            
+            <div className="hidden md:flex items-center justify-end xl:space-x-10 space-x-3">
+              <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                O projekcie
+              </a>
+              <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                Członkowie
+              </a>
+              <a href="#" className="bg-[#283593] text-white px-2 lg:px-4 py-2 rounded-xl hover:bg-blue-700 text-[14px] md:text-[16px]">
+                Sponsorzy
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StandardHeader
